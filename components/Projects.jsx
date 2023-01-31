58ccec2d2cb54f19abc864a8c7158c011fe84275
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-31 14:03:23 -0800

    Provided href for projectItem

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
-import sampleProject from "../public/assets/skills/html.png";
 
 const Projects = () => {
     return (
@@ -16,21 +15,21 @@ const Projects = () => {
                         type="NextJS"
                         imgSrc="https://raw.githubusercontent.com/pn-code/restaurant-app/main/gh/menu.png"
                         imgAlt="/"
-                        href="/"
+                        href="/restaurant"
                     />
                     <ProjectItem
                         title="Instagram Clone"
                         type="React / MERN Stack"
                         imgSrc="https://raw.githubusercontent.com/pn-code/ig-clone/main/gh-preview/ig-clone-preview-1.png"
                         imgAlt="/"
-                        href="/"
+                        href="/igclone"
                     />
                     <ProjectItem
                         title="Fitness Mobile"
                         type="React Native / Firebase"
                         imgSrc="https://raw.githubusercontent.com/pn-code/fitness-mobile/main/github/journal_screen.jpg"
                         imgAlt="/"
-                        href="/"
+                        href="/fitmobile"
                     />
                 </div>
             </div>
