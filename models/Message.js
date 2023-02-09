bf68d482c0cea6226b2d8ed80fb12eb12eb113f4
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-08 16:13:40 -0800

    Added model to handle messages

---
+++ models/Message.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+    name: { type: String, minLength: 3, maxLength: 30, required: true },
+    tele: { type: String, minLength: 10, maxLength: 15, required: true },
+    email: { type: String, minLength: 5, maxLength: 40, required: true },
+    title: { type: String, minLength: 3, maxLength: 30, required: true },
+    body: { type: String, minLength: 10, maxLength: 300, required: true },
+});
+
+module.exports =
+    mongoose.models.Message || mongoose.model("Message", MessageSchema);
