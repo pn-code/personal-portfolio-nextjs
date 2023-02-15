30363c4251b58c64f9632515ca03677be8651638
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-14 19:24:38 -0800

    Added input states & handler and handleLogin

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div className="w-full h-[600px] flex flex-col justify-center items-center pt-[80px]">
-    <div className="flex flex-col justify-center items-center">
-        <h1 className="mb-10">Login</h1>
-        <form className="flex flex-col gap-4">
-            <input className="p-2 rounded-md w-72" type="text" placeholder="username"/>
-            <input className="p-2 rounded-md w-72" type="text" placeholder="password"/>
-        <button className="h-12">Login</button>
-        </form>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("/api/login", { username, password });
+    console.log(res)
+  };
+
+  return (
+    <div className="w-full h-[600px] flex flex-col justify-center items-center pt-[80px]">
+      <h1 className="mb-10">Login</h1>
+      <form onSubmit={handleLogin} className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          className="p-2 rounded-md w-72"
+          type="text"
+          placeholder="username"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          className="p-2 rounded-md w-72"
+          type="password"
+          placeholder="password"
+        />
+        <button type="submit" className="h-12">
+          Login
+        </button>
+      </form>
     </div>
-  </div>;
+  );
 };
 
 export default Login;
