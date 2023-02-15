5e6e4a99f87a22db8c5b4d8d6d4ec3fe5dd31b96
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-14 21:11:32 -0800

    Added deleteMessage function

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -1,6 +1,7 @@
+import axios from "axios";
 import React, { useState } from "react";
 
-const MessageCard = ({ message }) => {
+const MessageCard = ({ message, setMessagesArray }) => {
   const [viewDetails, setViewDetails] = useState(false);
   const formattedDate = new Date(message.date.toString())
     .toLocaleString("en-US", {
@@ -9,20 +10,36 @@ const MessageCard = ({ message }) => {
     .toString()
     .split(",")[0];
 
+  const deleteMessage = async () => {
+    try {
+      const res = await axios.delete("/api/messages", {
+        data: { messageId: message._id },
+      });
+      setMessagesArray((messagesArray) =>
+        messagesArray.filter((prevMessage) => prevMessage._id !== message._id)
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <article
-      className="flex flex-col gap-2 bg-slate-300 rounded-md p-2"
-    >
+    <article className="flex flex-col gap-2 bg-slate-300 rounded-md p-2">
       <section className="flex justify-between items-center ">
         <h2 className="text-[16px]">{message.name}</h2>
         <span className="text-sm">{message.title.substring(0, 20)}</span>
         <span className="text-sm">{formattedDate}</span>
-        <button onClick={() => setViewDetails((prevViewDetails) => !prevViewDetails)} className="px-3 py-1">{viewDetails ? "Close Details": "View Details"}</button>
+        <button
+          onClick={() => setViewDetails((prevViewDetails) => !prevViewDetails)}
+          className="px-3 py-1 text-xs sm:text-[16px] sm:py-2"
+        >
+          {viewDetails ? "Close Details" : "View Details"}
+        </button>
       </section>
 
       {viewDetails && (
         <div className="flex flex-col gap-2 ">
-          <section>
+          <section className="w-96">
             <h4>Body: </h4>
             <p>{message.body}</p>
           </section>
@@ -35,11 +52,13 @@ const MessageCard = ({ message }) => {
             </div>
             <div>
               <h4>Email</h4>
-              <span>{message.tele}</span>
+              <span>{message.email}</span>
             </div>
           </section>
 
-          <button className="py-1 mt-10">Delete</button>
+          <button onClick={deleteMessage} type="button" className="py-1 mt-10">
+            Delete
+          </button>
         </div>
       )}
     </article>
