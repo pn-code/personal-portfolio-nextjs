784e6b7151fc4bd7be4ee8d1670440634f1be671
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-29 23:57:20 -0700

    Updated styles for project pages

--- pages/fitmobile.jsx
+++ pages/fitmobile.jsx
@@ -70,21 +70,21 @@ const FitMobile = () => {
                         <button className="px-8 py-2 mt-4">Demo (WIP)</button>
                     {/* </a> */}
                 </div>
-                <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4 px-4">
+                <div className="col-span-4 md:col-span-1 shadow-lg shadow-gray-400 bg-gray-500 rounded-xl py-4 px-4">
                     <div className="p-2">
                         <p className="text-center font-bold pb-2">
                             Technologies
                         </p>
                         <div className="grid grid-cols-3 md:grid-cols-1">
-                            <p className="text-gray-600 py-2 flex items-center">
+                            <p className="py-2 flex items-center">
                                 <RiRadioButtonFill className="pr-2" /> React
                                 Native
                             </p>
-                            <p className="text-gray-600 py-2 flex items-center">
+                            <p className="py-2 flex items-center">
                                 <RiRadioButtonFill className="pr-2" />{" "}
                                 Javascript
                             </p>
-                            <p className="text-gray-600 py-2 flex items-center">
+                            <p className="py-2 flex items-center">
                                 <RiRadioButtonFill className="pr-2" /> Firebase
                             </p>
                         </div>
--- pages/fitness.jsx
+++ pages/fitness.jsx
@@ -72,25 +72,25 @@ const Fitness = () => {
                         <button className="px-8 py-2 mt-4">Demo</button>
                     </a>
                 </div>
-                <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4 px-4">
+                <div className="col-span-4 md:col-span-1  shadow-lg shadow-gray-400 bg-gray-500  rounded-xl py-4 px-4">
                     <div className="p-2">
                         <p className="text-center font-bold pb-2">
                             Technologies
                         </p>
                         <div className="grid grid-cols-3 md:grid-cols-1">
-                            <p className="text-gray-600 py-2 flex items-center">
+                            <p className="py-2 flex items-center">
                                 <RiRadioButtonFill className="pr-2" /> React
                             </p>
-                            <p className="text-gray-600 py-2 flex items-center">
+                            <p className="py-2 flex items-center">
                                 <RiRadioButtonFill className="pr-2" /> Node
                             </p>
-                            <p className="text-gray-600 py-2 flex items-center">
+                            <p className="py-2 flex items-center">
                                 <RiRadioButtonFill className="pr-2" /> Express
                             </p>
-                            <p className="text-gray-600 py-2 flex items-center">
+                            <p className="py-2 flex items-center">
                                 <RiRadioButtonFill className="pr-2" /> MongoDB
                             </p>
-                            <p className="text-gray-600 py-2 flex items-center">
+                            <p className="py-2 flex items-center">
                                 <RiRadioButtonFill className="pr-2" /> Tailwind
                             </p>
                         </div>
