0b398fe28cdc33c7e418c3b0776bc4e1d19af331
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-29 23:56:14 -0700

    Updated navbar styles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,17 +11,17 @@ import navLogo from ".//../public/assets/navLogo.png";
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const [shadow, setShadow] = useState(false);
-    const [navBg, setNavBg] = useState("#f6f6f6");
-    const [linkColor, setLinkColor] = useState("black");
+    const [navBg, setNavBg] = useState("#181818");
+    const [linkColor, setLinkColor] = useState("ghostwhite");
     const router = useRouter();
 
     useEffect(() => {
         if (router.asPath === "/sample_project") {
-            setNavBg("transparent");
-            setLinkColor("#f6f6f6");
+            setNavBg("#181818");
+            setLinkColor("ghostwhite");
         } else {
-            setNavBg("#f6f6f6");
-            setLinkColor("black");
+            setNavBg("#181818");
+            setLinkColor("ghostwhite");
         }
     }, [router.asPath]);
 
@@ -49,9 +49,9 @@ const Navbar = () => {
                     : "fixed w-full h-20 z-[100]"
             }
         >
-            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-                <Link href="/">
-                    <Image src={navLogo} alt="" width={96} height={40} />
+            <div className="flex justify-between items-center w-full h-full px-4 2xl:px-16">
+                <Link className="text-2xl font-semibold" href="/">
+                    Philip Nguyen
                 </Link>
 
                 <div>
@@ -94,30 +94,19 @@ const Navbar = () => {
                 <div
                     className={
                         nav
-                            ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[white] p-10 ease-in duration-500"
+                            ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-gray-800 p-10 ease-in duration-500"
                             : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
                     }
                 >
                     <div>
                         <div className="flex w-full items-center justify-between">
-                            <Image
-                                src={navLogo}
-                                alt=""
-                                width="90"
-                                height="40"
-                            />
+                            <h1>Philip Nguyen</h1>
                             <div
-                                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+                                className="rounded-full shadow-lg shadow-gray-500 p-3 cursor-pointer bg-gray-300"
                                 onClick={handleNav}
                             >
-                                <AiOutlineClose />
+                                <AiOutlineClose color="black"/>
                             </div>
-                        </div>
-
-                        <div className="border-b border-gray-300 my-4">
-                            <p className="w-[85%] md:w-[90%] py-4">
-                                Let&apos;s build something together!
-                            </p>
                         </div>
 
                         <div className="py-4 flex flex-col">
@@ -125,7 +114,7 @@ const Navbar = () => {
                                 <Link href="/">
                                     <li
                                         onClick={() => setNav(false)}
-                                        className="py-4 text-sm font-semibold text-zinc-700"
+                                        className="py-4 text-sm font-semibold"
                                     >
                                         Home
                                     </li>
@@ -133,7 +122,7 @@ const Navbar = () => {
                                 <Link href="/#about">
                                     <li
                                         onClick={() => setNav(false)}
-                                        className="py-4 text-sm font-semibold text-zinc-700"
+                                        className="py-4 text-sm font-semibold"
                                     >
                                         About
                                     </li>
@@ -141,7 +130,7 @@ const Navbar = () => {
                                 <Link href="/#skills">
                                     <li
                                         onClick={() => setNav(false)}
-                                        className="py-4 text-sm font-semibold text-zinc-700"
+                                        className="py-4 text-sm font-semibold"
                                     >
                                         Skills
                                     </li>
@@ -149,7 +138,7 @@ const Navbar = () => {
                                 <Link href="/#projects">
                                     <li
                                         onClick={() => setNav(false)}
-                                        className="py-4 text-sm font-semibold text-zinc-700"
+                                        className="py-4 text-sm font-semibold"
                                     >
                                         Projects
                                     </li>
@@ -157,7 +146,7 @@ const Navbar = () => {
                                 <Link href="/#contact">
                                     <li
                                         onClick={() => setNav(false)}
-                                        className="py-4 text-sm font-semibold text-zinc-700"
+                                        className="py-4 text-sm font-semibold"
                                     >
                                         Contact
                                     </li>
