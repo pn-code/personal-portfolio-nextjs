995ae0abf27a5fb147ae5b89288e623c4d40b32c
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-29 23:57:00 -0700

    Implementing new links for project items

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,50 +3,51 @@ import ProjectItem from "./ProjectItem";
 import fitnessPreview from "../public/assets/projects/fitness-plans.png";
 import fitmobilePreview from "../public/assets/projects/fitmobile-preview.jpg";
 import restaurantPreview from "../public/assets/projects/restaurant-menu.png";
-import igClonePreview from "../public/assets/projects/ig-preview.png";
 
 const Projects = () => {
-    return (
-        <div id="projects" className="w-full">
-            <div className="max-w-[1240px] mx-auto px-2 py-16">
-                <p className="text-xl tracking-widest uppercase text-[#3855b6] font-semibold">
-                    Projects
-                </p>
-                <h2 className="py-4">What I&apos;ve Built</h2>
-                <div className="grid md:grid-cols-2 gap-8">
-                    {/* Add Latest Projects Here... */}
-                    <ProjectItem
-                        title="Restaurant App"
-                        type="NextJS"
-                        imgSrc={restaurantPreview}
-                        imgAlt="/"
-                        href="/restaurant"
-                    />
-                    <ProjectItem
-                        title="Fitness Mobile"
-                        type="React Native / Firebase"
-                        imgSrc={fitmobilePreview}
-                        imgAlt="/"
-                        href="/fitmobile"
-                    />
-                    <ProjectItem
-                        title="Fitness App"
-                        type="ReactJS / MERN Stack"
-                        imgSrc={fitnessPreview}
-                        imgAlt="/"
-                        href="/fitness"
-                    />
-                    <ProjectItem
-                        title="Instagram Clone"
-                        type="React / MERN Stack"
-                        imgSrc={igClonePreview}
-                        imgAlt="/"
-                        href="/igclone"
-                    />
-                </div>
-            </div>
+  return (
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-indigo-600 font-semibold">
+          Projects
+        </p>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          {/* Add Latest Projects Here... */}
+          <ProjectItem
+            title="Restaurant App"
+            type="NextJS"
+            imgSrc={restaurantPreview}
+            imgAlt="/"
+            href="/restaurant"
+            githubLink="https://github.com/pn-code/restaurant-app"
+            demoLink="https://restaurant.philipnguyen.dev/"
+            demo={true}
+          />
+          <ProjectItem
+            title="Fitness Mobile"
+            type="React Native / Firebase"
+            imgSrc={fitmobilePreview}
+            imgAlt="/"
+            href="/fitmobile"
+            githubLink="https://github.com/pn-code/fitness-mobile"
+            demoLink=""
+            demo={false}
+          />
+          <ProjectItem
+            title="Fitness App"
+            type="ReactJS / MERN Stack"
+            imgSrc={fitnessPreview}
+            imgAlt="/"
+            href="/fitness"
+            githubLink="https://github.com/pn-code/fitness-app"
+            demoLink="https://fitness.philipnguyen.dev/"
+            demo={true}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
