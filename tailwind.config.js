10fd3799ea63641c6bcc12f097a1aa0ed8d33538
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-17 17:20:02 -0700

    Updated global styles with new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f6f5f4",
+        secondary: "#060505",
+        primaryBtn: "#5125b6",
+        secondaryBtn: "#ededed",
+        accent: "#428bff",
+      },
+    },
   },
   plugins: [],
-}
+};
