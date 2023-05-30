3b66da079aba3b913c904d22b190642d68fd6abd
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-30 15:45:16 -0700

    Added key to projects mapper

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,7 +9,7 @@ const allProjects = [
     description:
       "FitHeroes is a powerful fitness app that helps you track your progress, plan your workouts, and achieve your fitness goals. It provides a variety of features including a detailed calorie calculator, weight and body fat tracker, nutrition and exercise journals, and the ability to build, view, and save workout plans.",
     skills: ["Next.js", "React", "PlanetScale", "TailwindCSS", "Zod", "Prisma"],
-    imageSrc: "/assets/projects/fitheroes-img.jpg"
+    imageSrc: "/assets/projects/fitheroes-img.jpg",
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ const allProjects = [
     description:
       "The Restaurant App is a full-stack web application built with Next.js, React, Redux, MongoDB, and TailwindCSS. It offers a user-friendly interface for customers to browse the menu, customize orders with add-ons and combos, manage their cart, place orders (PayPal or cash), and track order status. Admins can access the dashboard to process orders and update details.",
     skills: ["Next.js", "React", "TailwindCSS", "MongoDB", "Redux"],
-    imageSrc: "/assets/projects/monsterburger-img.png"
+    imageSrc: "/assets/projects/monsterburger-img.png",
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const allProjects = [
     description:
       "The Bug Tracker is a Next.js project designed to keep track of bugs and monitor progress made towards fixing them across multiple projects. It provides a user-friendly interface for managing projects, tracking issues, and facilitating communication among team members.",
     skills: ["Next.js", "React", "TailwindCSS", "PostgreSQL"],
-    imageSrc: "/assets/projects/bugtracker-img.png"
+    imageSrc: "/assets/projects/bugtracker-img.png",
   },
 ];
 
@@ -47,6 +47,7 @@ const Projects = () => {
         <div className="flex flex-col gap-4">
           {allProjects.map((project) => (
             <ProjectCard
+              key={project.id}
               title={project.title}
               description={project.description}
               skills={project.skills}
