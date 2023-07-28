f468e74d1fe73a568cdc89da7ba99af8c1204bd0
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-28 16:51:57 -0700

    Allow users to scroll through sections

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,44 +1,57 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
+import About from "../components/About";
+import Projects from "../components/Projects";
+import Footer from "../components/Footer";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Philip Nguyen</title>
-        <meta
-          name="description"
-          content="Philip Nguyen, a Full Stack Developer"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-      <Navbar />
+    return (
+        <>
+            <Head>
+                <title>Philip Nguyen</title>
+                <meta
+                    name="description"
+                    content="Philip Nguyen, a Full Stack Developer"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+                <link
+                    rel="mask-icon"
+                    href="/favicon/safari-pinned-tab.svg"
+                    color="#5bbad5"
+                />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
+            <Navbar />
 
-      <main className="px-4">
-        <Main />
-      </main>
-    </>
-  );
+            <main className="px-4">
+                <Main />
+                <About />
+                <Projects />
+                <Footer />
+            </main>
+        </>
+    );
 }
