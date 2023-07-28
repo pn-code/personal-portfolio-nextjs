963008d4411dc8cd345794123d1f8b4c88c2835f
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-28 16:49:50 -0700

    Added lottie animation

---
+++ components/animations/WebAnimation.jsx
@@ -0,0 +1,10 @@
+import Lottie from "lottie-react";
+import webAnimation from "./web.json";
+
+export default function WebAnimation() {
+    const style = {
+        height: 360,
+    };
+
+    return <Lottie style={style} animationData={webAnimation} speed={0.1} />;
+}
