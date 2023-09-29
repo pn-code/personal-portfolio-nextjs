80dd29022843887df8cfa072c286170639b7a321
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-29 14:31:38 -0700

    Added height & width to make buttons circular

--- components/portfolio/Footer.tsx
+++ components/portfolio/Footer.tsx
@@ -4,12 +4,12 @@ import { Button } from "../ui/button";
 
 export default function Footer() {
   return (
-    <footer id="footer" className="w-full text-center mb-5">
-      <Button className="mb-5" variant="ghost">
-        <a href="#">
-          <ChevronsUp />
-        </a>
-      </Button>
+    <footer id="footer" className="w-full h-full text-center">
+      <a href="#">
+        <Button className="h-12 w-12 mb-5 rounded-full p-1">
+          <ChevronsUp size={30} />
+        </Button>
+      </a>
 
       <h2>Contact Me</h2>
       <div className="flex justify-center gap-4 max-w-[330px] m-auto pb-4 pt-4">
@@ -18,13 +18,13 @@ export default function Footer() {
           target="_blank"
           rel="noreferrer"
         >
-          <Button className="rounded-full shadow-lg shadow-gray-900 p-2 sm:p-3 cursor-pointer">
+          <Button className="rounded-full bg-slate-900 dark:bg-slate-50 p-1 h-12 w-12 dark:hover:bg-slate-200">
             <Linkedin size={24} />
           </Button>
         </a>
 
         <a href="https://github.com/pn-code" target="_blank" rel="noreferrer">
-          <Button className="rounded-full shadow-lg shadow-gray-900 p-2 sm:p-3 cursor-pointer">
+          <Button className="rounded-full bg-slate-900 dark:bg-slate-50 p-1 h-12 w-12 dark:hover:bg-slate-200">
             <Github size={24} />
           </Button>
         </a>
@@ -34,7 +34,7 @@ export default function Footer() {
           target="_blank"
           rel="noreferrer"
         >
-          <Button className="rounded-full shadow-lg shadow-gray-900 p-2 sm:p-3 cursor-pointer">
+          <Button className="rounded-full bg-slate-900 dark:bg-slate-50 p-1 h-12 w-12 dark:hover:bg-slate-200">
             <Mail size={24} />
           </Button>
         </a>
