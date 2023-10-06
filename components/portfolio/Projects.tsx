c1d6467f3c0552b4093e8ead662d9b213ffbf69c
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-06 11:36:28 -0700

    Cleaned up images folder & added new images

--- components/portfolio/Projects.tsx
+++ components/portfolio/Projects.tsx
@@ -9,7 +9,7 @@ const allProjects = [
     description:
       "FitHeroes is a powerful fitness app that helps you track your progress, plan your workouts, and achieve your fitness goals. It provides a variety of features including a detailed calorie calculator, weight and body fat tracker, nutrition and exercise journals, and the ability to build, view, and save workout plans.",
     skills: ["TypeScript", "React (Next.js)", "Node", "MySQL"],
-    imageSrc: "/assets/projects/fitheroes-img.jpg",
+    imageSrc: "/assets/projects/fitheroes.jpg",
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ const allProjects = [
     description:
       "Monster Burger is a restaurant web app which offers a user-friendly interface for customers to browse the menu, customize orders with add-ons and combos, manage their cart, place orders (PayPal or cash), and track order status. Admins can access the dashboard to process orders and update details.",
     skills: ["JavaScript", "React (Next.js)", "Redux", "Node", "MongoDB"],
-    imageSrc: "/assets/projects/monsterburger-img.png",
+    imageSrc: "/assets/projects/monsterburger.png",
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const allProjects = [
     description:
       "The Bug Tracker is designed to keep track of bugs and monitor progress made towards fixing them across multiple projects. It provides a user-friendly interface for managing projects, tracking issues, and facilitating communication among team members.",
     skills: ["JavaScript", "React (Next.js)", "Node", "PostgreSQL"],
-    imageSrc: "/assets/projects/bugtracker-img.png",
+    imageSrc: "/assets/projects/bug.jpg",
   },
 ];
 
