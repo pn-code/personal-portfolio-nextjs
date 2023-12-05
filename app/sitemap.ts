22ab00c7e8e2abc86eae576f6448bd064e46ec71
Philip Nguyen <philipnguyen953@gmail.com>
2023-12-04 18:30:52 -0800

    Removed other domains & updated datestring to date

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,19 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://philipnguyen.dev",
-      lastModified: new Date().toString(),
-    },
-    {
-      url: "https://restaurant.philipnguyen.dev",
-      lastModified: new Date().toString(),
-    },
-    {
-      url: "https://fitheroes.philipnguyen.dev",
-      lastModified: new Date().toString(),
-    },
-    {
-      url: "https://shop.philipnguyen.dev",
-      lastModified: new Date().toString(),
+      lastModified: new Date(),
     },
   ];
 }
